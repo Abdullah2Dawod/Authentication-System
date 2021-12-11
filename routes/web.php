151d1bf3e31cb2c299ccc5e3a9d1402fafccc7ec
Abood5418918 <abd.99999.omar@gmail.com>
2021-12-11 15:12:43 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\TaskController;
+
+
+Route::get("/", [TaskController::class, 'index']);
+
+Route::post("/store", [TaskController::class, 'store']);
+
+Route::post("/delete/{id}", [TaskController::class, 'delete']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+ ?>
